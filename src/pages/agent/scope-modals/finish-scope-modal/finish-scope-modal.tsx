a187556cb5a527c40b60b6a8b5a067c96e7f252f
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-08-31 11:26:17 +0300

    feat: add force finish checkbox in finish scope modal

--- src/pages/agent/scope-modals/finish-scope-modal/finish-scope-modal.tsx
+++ src/pages/agent/scope-modals/finish-scope-modal/finish-scope-modal.tsx
@@ -16,7 +16,7 @@
 import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import {
-  Button, Inputs, Popup, GeneralAlerts, Spinner,
+  Button, Icons, Popup, GeneralAlerts, Spinner, Formik, Form, Checkbox, Field,
 } from "@drill4j/ui-kit";
 import { sendNotificationEvent } from "@drill4j/send-notification-event";
 import { useCloseModal } from "@drill4j/common-hooks";
@@ -34,9 +34,8 @@ export const FinishScopeModal = () => {
   const {
     agentId = "", pluginId = "",
   } = useAgentRouteParams();
-  const { testTypes = [] } = useBuildVersion<ActiveSessions>("/active-scope/summary/active-sessions") || {};
+  const { testTypes: activeSessionTest = [] } = useBuildVersion<ActiveSessions>("/active-scope/summary/active-sessions") || {};
   const [errorMessage, setErrorMessage] = useState("");
-  const [ignoreScope, setIgnoreScope] = useState(false);
   const [loading, setLoading] = useState(false);
   const testsCount = scope
     ? (scope.coverage.byTestType || []).reduce((acc, { summary: { testCount = 0 } }) => acc + testCount, 0)
@@ -44,6 +43,8 @@ export const FinishScopeModal = () => {
   const { push, location: { pathname = "" } } = useHistory();
   const isScopeInfoPage = scope?.id && pathname.includes(scope.id);
   const closeModal = useCloseModal("/finish-scope-modal");
+
+  if (!scope) return null;
 
   return (
     <Popup
@@ -63,7 +64,7 @@ export const FinishScopeModal = () => {
             {errorMessage}
           </GeneralAlerts>
         )}
-        {testTypes.length > 0 && (
+        {activeSessionTest.length > 0 && (
           <GeneralAlerts type="WARNING">
             <div>
               At least one active session has been detected.<br />
@@ -77,50 +78,70 @@ export const FinishScopeModal = () => {
             </div>
           </GeneralAlerts>
         )}
-        {Boolean(!testsCount && !testTypes.length) && (
+        {Boolean(!testsCount && !activeSessionTest.length) && (
           <GeneralAlerts type="WARNING">
             Scope is empty and will be deleted after finishing.
           </GeneralAlerts>
         )}
-        <div tw="m-6">
-          <ScopeSummary scope={scope as ActiveScope} testsCount={testsCount} />
-          <div tw="mt-6 mb-9">
-            <Label
-              onClick={() => setIgnoreScope(!ignoreScope)}
-              disabled={!testsCount || testTypes.length > 0}
-            >
-              <Inputs.Checkbox
-                tw="text-blue-default"
-                checked={ignoreScope}
-              />
-              <span tw="text-14 leading-20">Ignore scope in build stats</span>
-            </Label>
-          </div>
-          <div className="flex items-center gap-x-4 w-full mt-9">
-            {!testTypes.length ? (
-              <>
+        <Formik
+          initialValues={{ ignoreScope: false, forceFinish: false }}
+          onSubmit={async ({ ignoreScope, forceFinish }: any) => {
+            setLoading(true);
+            await finishScope(agentId, pluginId, {
+              onSuccess: () => {
+                sendNotificationEvent({ type: "SUCCESS", text: "Scope has been finished" });
+                closeModal();
+              },
+              onError: setErrorMessage,
+            })({ prevScopeEnabled: !ignoreScope, savePrevScope: true, forceFinish });
+            isScopeInfoPage && !scope?.sessionsFinished &&
+                  push(getPagePath({ name: "test2code" }));
+            setLoading(false);
+          }}
+        >
+          {({ values: { forceFinish } }) => (
+            <Form tw="m-6">
+              <ScopeSummary scope={scope as ActiveScope} testsCount={testsCount} />
+              <div tw="flex flex-col gap-y-4 mt-6 mb-9 text-14 leading-20 text-blue-default">
+                {Boolean(activeSessionTest.length) && (
+                  <div>
+                    <Label disabled={false}>
+                      <Field
+                        type="checkbox"
+                        name="forceFinish"
+                      >
+                        {({ field }: any) => (<Checkbox field={field} />)}
+                      </Field>
+                      <span tw="text-monochrome-black">Delete active sessions and finish scope anyway</span>
+                    </Label>
+                    {forceFinish && !scope.coverage.percentage && (
+                      <div tw="flex gap-x-2 items-center mt-2 ml-6 text-orange-default font-regular">
+                        <Icons.Warning /> Scope is empty and will be deleted after finishing
+                      </div>
+                    )}
+                  </div>
+                )}
+                <Label disabled={!testsCount || activeSessionTest.length > 0}>
+                  <Field
+                    type="checkbox"
+                    name="ignoreScope"
+                  >
+                    {({ field }: any) => (<Checkbox field={field} />)}
+                  </Field>
+                  <span tw="text-monochrome-black">Ignore scope in build stats</span>
+                </Label>
+              </div>
+              <div className="flex items-center gap-x-4 w-full mt-9">
                 <Button
                   className={`flex justify-center items-center gap-x-1 ${testsCount ? "w-30" : "w-40"}`}
                   primary
                   size="large"
-                  disabled={testTypes.length > 0 || loading}
-                  onClick={async () => {
-                    setLoading(true);
-                    await finishScope(agentId, pluginId, {
-                      onSuccess: () => {
-                        sendNotificationEvent({ type: "SUCCESS", text: "Scope has been finished" });
-                        closeModal();
-                      },
-                      onError: setErrorMessage,
-                    })({ prevScopeEnabled: !ignoreScope, savePrevScope: true });
-                    isScopeInfoPage && !scope?.sessionsFinished &&
-                        push(getPagePath({ name: "test2code" }));
-                    setLoading(false);
-                  }}
+                  disabled={loading || (Boolean(activeSessionTest.length) && !forceFinish)}
+                  type="submit"
                   data-test="finish-scope-modal:finish-scope-button"
                 >
                   {loading && <Spinner disabled />}
-                  {!loading && Boolean(testsCount) && "Finish Scope" }
+                  {!loading && Boolean(testsCount) && "Finish Scope"}
                   {!loading && !testsCount && "Finish and Delete" }
                 </Button>
                 <Button
@@ -132,19 +153,10 @@ export const FinishScopeModal = () => {
                 >
                   Cancel
                 </Button>
-              </>
-            ) : (
-              <Button
-                secondary
-                size="large"
-                onClick={closeModal}
-                data-test="finish-scope-modal:cancel-modal-button"
-              >
-                Ok, got it
-              </Button>
-            )}
-          </div>
-        </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </Popup>
   );
