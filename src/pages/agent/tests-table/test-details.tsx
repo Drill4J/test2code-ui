fac6e1189b4491a3fbf60c9e33b30f8fbcda9af3
ilia_solovev1 <ilia_solovev1@epam.com>
2021-11-26 15:56:04 +0300

    build:  add memo to TestDetails

--- src/pages/agent/tests-table/test-details.tsx
+++ src/pages/agent/tests-table/test-details.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from "react";
+import React, { useMemo } from "react";
 import {
   Icons, Stub, Table, Cells, Link,
 } from "@drill4j/ui-kit";
@@ -34,96 +34,98 @@ interface Props {
   topicCoveredMethodsByTest: string;
 }
 
+const columns = [
+  {
+    Header: "Name",
+    accessor: "overview.details.name",
+    textAlign: "left",
+    filterable: true,
+  },
+  {
+    Header: "Path",
+    accessor: "overview.details.path",
+    textAlign: "left",
+    filterable: true,
+  },
+  {
+    Header: "Test type",
+    accessor: "type",
+    Cell: ({ value }: any) => (
+      <>
+        {capitalize(value)}
+      </>
+    ),
+    textAlign: "left",
+  },
+  {
+    Header: "Status",
+    accessor: "overview.result",
+    Cell: ({ value }: any) => (
+      <Cells.TestStatus
+        tw="inline"
+        type={value}
+      >
+        {capitalize(value)}
+      </Cells.TestStatus>
+    ),
+    textAlign: "left",
+  },
+  {
+    Header: "Coverage, %",
+    accessor: "coverage.percentage",
+    Cell: Cells.Coverage,
+  },
+  {
+    Header: "Methods covered",
+    accessor: "coverage.methodCount.covered",
+    Cell: ({ value, row: { original: { id = "", coverage: { methodCount: { covered = 0 } = {} } = {} } = {} } = {} }: any) => (
+      <Cells.Clickable
+        tw="inline"
+        data-test="test-actions:view-curl:id"
+        disabled={!value}
+      >
+        <Link to={getModalPath({ name: "coveredMethods", params: { coveredMethods: covered, testId: id } })}>
+          {value}
+        </Link>
+      </Cells.Clickable>
+    ),
+  },
+  {
+    Header: "Duration",
+    accessor: "overview.duration",
+    Cell: Cells.Duration,
+  }];
+
 export const TestDetails = ({
   tests: { items: tests = [] },
 }: Props) => {
   const { agentId } = useAgentRouteParams();
   const { status } = useAgent(agentId);
 
+  const stub = useMemo(() => (tests.length > 0
+    ? (
+      <Stub
+        icon={<Icons.Test height={104} width={107} />}
+        title="No results found"
+        message="Try adjusting your search or filter to find what you are looking for."
+      />
+    )
+    : (
+      <Stub
+        icon={<Icons.Test height={104} width={107} />}
+        title={status === AGENT_STATUS.BUSY ? "Build tests are loading" : "No tests available yet"}
+        message={status === AGENT_STATUS.BUSY
+          ? "It may take a few seconds."
+          : "Information about project tests will appear after the first launch of tests."}
+      />
+    )), [tests.length]);
+  const data = useMemo(() => transformTests(tests), [JSON.stringify(tests)]);
   return (
     <div tw="flex flex-col mt-12" data-test="test-details:table-wrapper">
       <Table
-        data={transformTests(tests)}
-        columns={[
-          {
-            Header: "Name",
-            accessor: "overview.details.name",
-            textAlign: "left",
-            filterable: true,
-          },
-          {
-            Header: "Path",
-            accessor: "overview.details.path",
-            textAlign: "left",
-            filterable: true,
-          },
-          {
-            Header: "Test type",
-            accessor: "type",
-            Cell: ({ value }: any) => (
-              <>
-                {capitalize(value)}
-              </>
-            ),
-            textAlign: "left",
-          },
-          {
-            Header: "Status",
-            accessor: "overview.result",
-            Cell: ({ value }: any) => (
-              <Cells.TestStatus
-                tw="inline"
-                type={value}
-              >
-                {capitalize(value)}
-              </Cells.TestStatus>
-            ),
-            textAlign: "left",
-          },
-          {
-            Header: "Coverage, %",
-            accessor: "coverage.percentage",
-            Cell: Cells.Coverage,
-          },
-          {
-            Header: "Methods covered",
-            accessor: "coverage.methodCount.covered",
-            Cell: ({ value, row: { original: { id = "", coverage: { methodCount: { covered = 0 } = {} } = {} } = {} } = {} }: any) => (
-              <Cells.Clickable
-                tw="inline"
-                data-test="test-actions:view-curl:id"
-                disabled={!value}
-              >
-                <Link to={getModalPath({ name: "coveredMethods", params: { coveredMethods: covered, testId: id } })}>
-                  {value}
-                </Link>
-              </Cells.Clickable>
-            ),
-          },
-          {
-            Header: "Duration",
-            accessor: "overview.duration",
-            Cell: Cells.Duration,
-          }]}
-        stub={
-          tests.length > 0
-            ? (
-              <Stub
-                icon={<Icons.Test height={104} width={107} />}
-                title="No results found"
-                message="Try adjusting your search or filter to find what you are looking for."
-              />
-            )
-            : (
-              <Stub
-                icon={<Icons.Test height={104} width={107} />}
-                title={status === AGENT_STATUS.BUSY ? "Build tests are loading" : "No tests available yet"}
-                message={status === AGENT_STATUS.BUSY
-                  ? "It may take a few seconds."
-                  : "Information about project tests will appear after the first launch of tests."}
-              />
-            )
-        }
+        data={data}
+        columns={columns}
+        stub={stub}
       />
     </div>
   );
