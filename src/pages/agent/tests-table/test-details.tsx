3a49ee8e6ea74dd64f658c1f5835f2c2db000016
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-10 11:50:28 +0300

    feat: add render props for tests table

--- src/pages/agent/tests-table/test-details.tsx
+++ src/pages/agent/tests-table/test-details.tsx
@@ -96,6 +96,15 @@ const columns = [
     Cell: Cells.Duration,
   }];
 
+const renderProps = {
+  header: (title: string, pageLength: number, dataLength: number) => (
+    <div tw="flex justify-between text-monochrome-default text-14 leading-24 pb-3">
+      <div tw="uppercase font-bold">{title}</div>
+      <div>{`Displaying ${pageLength} of ${dataLength} tests`}</div>
+    </div>
+  ),
+};
+
 export const TestDetails = ({
   tests: { items: tests = [] },
 }: Props) => {
