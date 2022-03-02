60625fb47def10460c0922d149c77a6bcce25ee2
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-03-02 21:35:23 +0300

    fix: text style in subheader for associated tests modals

--- src/modules/associated-test-modal/associated-test-modal.tsx
+++ src/modules/associated-test-modal/associated-test-modal.tsx
@@ -56,6 +56,7 @@ export const AssociatedTestModal = () => {
             skeleton={isSkeleton}
             title={packageName}
             data-test="associated-tests:package-name"
+            tw="lowercase"
           >
             {isSkeleton ? "" : packageName || "-"}
           </MethodInfoValue>
@@ -63,6 +64,7 @@ export const AssociatedTestModal = () => {
             skeleton={isSkeleton}
             title={testClassName}
             data-test="associated-tests-modal:test-class-name"
+            tw="first-letter:uppercase"
           >
             {isSkeleton ? "" : testClassName || "-"}
           </MethodInfoValue>
@@ -204,6 +206,6 @@ const MethodInfoLabel = styled.div(tw`min-w-32px text-left text-14 leading-32 fo
 
 const MethodInfoValue = styled.div(({ skeleton }: { skeleton?: boolean }) =>
   [
-    tw`text-monochrome-default text-14 leading-20 break-all text-ellipsis lowercase`,
+    tw`text-monochrome-default text-14 leading-20 break-all text-ellipsis`,
     skeleton && tw`h-4 animate-pulse w-full bg-monochrome-medium-tint rounded`,
   ]);
