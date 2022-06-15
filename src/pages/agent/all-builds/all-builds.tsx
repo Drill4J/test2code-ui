7b4b3c138eded80d6041434381b9cc390a3c3df9
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-06-15 15:32:26 +0300

    feat: add flag icon for baseline build

--- src/pages/agent/all-builds/all-builds.tsx
+++ src/pages/agent/all-builds/all-builds.tsx
@@ -17,18 +17,23 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import {
-  Table, Typography, dateTimeFormatter, Stub, Icons,
+  Table, Typography, dateTimeFormatter, Stub, Icons, Tooltip,
 } from "@drill4j/ui-kit";
 import { BuildVersion } from "@drill4j/types-admin";
 import tw, { styled } from "twin.macro";
 
-import { useAdminConnection, useAgentRouteParams, useNavigation } from "hooks";
+import {
+  useActiveBuild, useAdminConnection, useAgentRouteParams, useBuildVersion, useNavigation,
+} from "hooks";
 import { PageHeader } from "components";
 
 export const AllBuilds = () => {
   const { agentId } = useAgentRouteParams();
   const { getPagePath } = useNavigation();
   const buildVersions = useAdminConnection<BuildVersion[]>(`/agents/${agentId}/builds/summary`) || [];
+  const { buildVersion: activeBuildVersion = "" } = useActiveBuild(agentId) || {};
+  const { version: baseline } = useBuildVersion("/data/baseline", { buildVersion: activeBuildVersion }) || {};
+  const isBaseline = (build: string) => build === baseline;
 
   return (
     <div tw="flex flex-col flex-grow">
@@ -61,6 +66,21 @@ export const AllBuilds = () => {
                       <span>{buildVersion}</span>
                     </Typography.MiddleEllipsis>
                   </Link>
+                  {isBaseline(buildVersion) && (
+                    <Tooltip
+                      position="top-right"
+                      message={(
+                        <>
+                          <div tw="font-bold">This build is set as baseline.</div>
+                          <div>
+                            All subsequent builds are compared with it.
+                          </div>
+                        </>
+                      )}
+                    >
+                      <Icons.Flag />
+                    </Tooltip>
+                  )}
                 </NameCell>
               ),
               textAlign: "left",
@@ -105,6 +125,7 @@ export const AllBuilds = () => {
               message="Try adjusting your search or filter to find what you are looking for."
             />
           )}
+          columnsDependency={[baseline]}
         />
       </div>
     </div>
